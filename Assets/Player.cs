d3573394daa311dd8849d44fedaa19d029a83d86
nmuddkidd <Nmuddkidd543@gmail.com>
2025-11-13 16:54:57 -0500

    camera controls

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float Xsens = 1f;
+    public float Ysens = 1f;
     private Rigidbody body;
     void Start()
     {
@@ -17,15 +19,21 @@ public class Player : MonoBehaviour
 
     void movement()
     {
-        if (Input.GetKey(KeyCode.W)){
-            body.linearVelocity = transform.forward * moveSpeed;
-        }else if (Input.GetKey(KeyCode.S)){
-            body.linearVelocity = transform.forward * -1 * moveSpeed;
+        if (Input.GetKey(KeyCode.W))
+        {
+            body.linearVelocity = new Vector3(body.linearVelocity.x, body.linearVelocity.y, moveSpeed);
         }
-        if (Input.GetKey(KeyCode.A)){
-            body.linearVelocity = transform.right * -1 * moveSpeed;
-        }else if (Input.GetKey(KeyCode.D)){
-            body.linearVelocity = transform.right * moveSpeed;
+        else if (Input.GetKey(KeyCode.S))
+        {
+            body.linearVelocity = new Vector3(body.linearVelocity.x, body.linearVelocity.y, -1 * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            body.linearVelocity = new Vector3(-1 * moveSpeed, body.linearVelocity.y, body.linearVelocity.z);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            body.linearVelocity = new Vector3(moveSpeed, body.linearVelocity.y, body.linearVelocity.z);
         }
     }
 }
--- Assets/follower.cs
+++ Assets/follower.cs
@@ -3,13 +3,22 @@ using UnityEngine;
 public class follower : MonoBehaviour
 {
     public GameObject seek;
+    public float Xsens = 1f;
+    public float Ysens = 1f;
+    private float pitch = 0;
+    private float yaw = 0;
     void Start()
     {
-        
+
     }
 
     void Update()
     {
         transform.position = seek.transform.position;
+                
+        pitch += Input.GetAxis("Mouse Y") * Ysens * -1;
+        yaw += Input.GetAxis("Mouse X") * Xsens;
+        transform.rotation = Quaternion.Euler(pitch,yaw,0);
+        transform.rotation = Quaternion.Euler(pitch,yaw,0);
     }
 }

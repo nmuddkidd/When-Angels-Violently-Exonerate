00bf209e67b15d811da09b06051b00145368604e
Nathan Mudd <nmuddkidd543@gmail.com>
2025-11-14 16:10:44 -0500

    omnidirectional movement & jumping

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,7 +6,10 @@ public class Player : MonoBehaviour
     public float moveSpeed = 5f;
     public float Xsens = 1f;
     public float Ysens = 1f;
+    public float jumpvelocity = 20f;
     private Rigidbody body;
+    private float yaw = 0;
+    private float pitch = 0;
     void Start()
     {
         body = GetComponent<Rigidbody>();
@@ -19,21 +22,32 @@ public class Player : MonoBehaviour
 
     void movement()
     {
+        pitch += Input.GetAxis("Mouse Y") * Ysens * -1;
+        yaw += Input.GetAxis("Mouse X") * Xsens;
+        transform.rotation = Quaternion.Euler(pitch,yaw,0);
+
+        float xrotation = transform.eulerAngles.y * Mathf.PI/180;
+        Debug.Log(xrotation);
+
         if (Input.GetKey(KeyCode.W))
         {
-            body.linearVelocity = new Vector3(body.linearVelocity.x, body.linearVelocity.y, moveSpeed);
+            body.linearVelocity = new Vector3(moveSpeed * Mathf.Sin(xrotation), body.linearVelocity.y, moveSpeed * Mathf.Cos(xrotation));
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            body.linearVelocity = new Vector3(body.linearVelocity.x, body.linearVelocity.y, -1 * moveSpeed);
+            body.linearVelocity = new Vector3(-1 * moveSpeed * Mathf.Sin(xrotation), body.linearVelocity.y, -1 * moveSpeed * Mathf.Cos(xrotation));
         }
         if (Input.GetKey(KeyCode.A))
         {
-            body.linearVelocity = new Vector3(-1 * moveSpeed, body.linearVelocity.y, body.linearVelocity.z);
+            body.linearVelocity = new Vector3(-1 * moveSpeed * Mathf.Cos(xrotation), body.linearVelocity.y, moveSpeed * Mathf.Sin(xrotation));
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            body.linearVelocity = new Vector3(moveSpeed, body.linearVelocity.y, body.linearVelocity.z);
+            body.linearVelocity = new Vector3(moveSpeed * Mathf.Cos(xrotation), body.linearVelocity.y, -1 * moveSpeed * Mathf.Sin(xrotation));
+        }
+
+        if(Input.GetKey(KeyCode.Space)){
+            body.linearVelocity = new Vector3(body.linearVelocity.x, jumpvelocity , body.linearVelocity.z);
         }
     }
 }
--- Assets/follower.cs
+++ Assets/follower.cs
@@ -5,8 +5,8 @@ public class follower : MonoBehaviour
     public GameObject seek;
     public float Xsens = 1f;
     public float Ysens = 1f;
-    private float pitch = 0;
     private float yaw = 0;
+    private float pitch = 0;
     void Start()
     {
 
@@ -18,7 +18,6 @@ public class follower : MonoBehaviour
                 
         pitch += Input.GetAxis("Mouse Y") * Ysens * -1;
         yaw += Input.GetAxis("Mouse X") * Xsens;
-        transform.rotation = Quaternion.Euler(pitch,yaw,0);
         transform.rotation = Quaternion.Euler(pitch,yaw,0);
     }
 }
